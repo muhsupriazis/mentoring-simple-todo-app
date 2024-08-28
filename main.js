93b2f47981d925ea9a4578afc0c9913d02f2b2d5
shixbt <nanashieth@gmail.com>
2024-08-28 15:26:29 +0800

    update data

--- main.js
+++ main.js
@@ -19,6 +19,13 @@ const functionDeleteData = (item) => {
   return dataDeleted;
 }
 
+// ini function untuk mengedite data
+const functionEditData = (item, newValue) => {
+  const dataEdited = data.map((value) => value === item ? newValue : value);
+  data = dataEdited;
+  return dataEdited;
+}
+
 // ini function untuk menampilkan data
 const functionShowData = (data) => {
   tampil.innerHTML = '';
@@ -26,9 +33,17 @@ const functionShowData = (data) => {
     const li = document.createElement('li');
     const span = document.createElement('span')
     const buttonDelete = document.createElement('button');
+    const buttonEdit = document.createElement('button-edit');
     buttonDelete.innerHTML = 'Delete';
+    buttonEdit.innerHTML = 'Edit';
     buttonDelete.className = 'bg-red-500 text-white p-1 rounded';
+    buttonEdit.className = 'bg-blue-500 text-white p-1 rounded';
     span.innerHTML = item;
+    buttonEdit.onclick = () => {
+      input.value = item;
+      buttonTambah.innerHTML = 'simpan';
+      buttonTambah.value = item;
+    }
     // ini event ketika button delete di klik
     buttonDelete.onclick = () => {
       const dataDeleted = functionDeleteData(item);
@@ -37,14 +52,23 @@ const functionShowData = (data) => {
     }
     li.appendChild(span);
     li.appendChild(buttonDelete);
+    li.appendChild(buttonEdit)
     tampil.appendChild(li);
   });
 }
 
 // ini event ketika button tambah di klik
 buttonTambah.addEventListener('click', () => {
-  const inputText = input.value;
-  const dataAdded = functionAddData(inputText);
-  functionShowData(dataAdded);
-  input.value = '';
+  if(buttonTambah.innerHTML == 'tambahkan') {
+    const inputText = input.value;
+    const dataAdded = functionAddData(inputText);
+    functionShowData(dataAdded);
+    input.value = '';
+  } else {
+    const inputText = input.value;
+    const dataEdited = functionEditData(buttonTambah.value, inputText);
+    functionShowData(dataEdited);
+    input.value = '';
+    buttonTambah.innerHTML = 'tambahkan';
+  }
 })
